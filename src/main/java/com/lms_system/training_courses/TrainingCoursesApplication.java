3b0c359411e599b590d5acfa31cb8385b796e3fa
Давид <bullatsev@gmail.com>
2025-03-13 01:07:05 +0300

    initial commit

---
+++ src/main/java/com/lms_system/training_courses/TrainingCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.lms_system.training_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingCoursesApplication.class, args);
+	}
+
+}

f4c855e799ca635c05297c748db4b68ba5308bc5
Egor2341 <egor.anti.00@mail.ru>
2025-03-13 11:00:37 +0300

    Launching an application and database in Docker

---
+++ src/main/java/com/lms_system/training_courses/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.lms_system.training_courses.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class Controller {
+
+    @GetMapping("")
+    public String homePage() {
+        return "Hello!";
+    }
+}
